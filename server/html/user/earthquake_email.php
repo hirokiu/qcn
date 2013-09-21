7fd3a18c44c8d57dd2d2a5aa82445e8dc15250b3
Carl Christensen <carlgt1@yahoo.com>
2013-09-21 15:23:08 -0400

    added some server side file e.g. plan_class_spec.xml

--- server/html/user/earthquake_email.php
+++ server/html/user/earthquake_email.php
@@ -1,15 +1,15 @@
 <?php
 chdir("/var/www/boinc/sensor/html/user");
 require_once("/var/www/boinc/sensor/html/inc/earthquake_email.inc");
-$mag  = 2.6; 
-$longitude = 174.853; 
-$latitude = -41.191; 
-$depth = 29.1; 
-$n_stations = 10; 
-$etime = 1378296253.989400; 
-$dtime = 1378296269; 
-$dt_detect  = 15.0; 
-$edir       = 1378296253; 
+$mag  = 3.6; 
+$longitude = 172.494; 
+$latitude = -43.465; 
+$depth = 2.2; 
+$n_stations = 8; 
+$etime = 1379651011.251209; 
+$dtime = 1379651030; 
+$dt_detect  = 18.7; 
+$edir       = 1379651011; 
 
 earthquake_email($mag,$longitude,$latitude,$depth,$n_stations,$etime,$edir,$dtime,$dt_detect);
 
