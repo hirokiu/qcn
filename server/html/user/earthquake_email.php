e8ea987bd8ec1bb8eaedc65cfdfe2cb8800599d8
Carl Christensen <carlgt1@yahoo.com>
2013-09-23 15:46:27 -0400

    server php for remote triggers

--- server/html/user/earthquake_email.php
+++ server/html/user/earthquake_email.php
@@ -1,15 +1,15 @@
 <?php
 chdir("/var/www/boinc/sensor/html/user");
 require_once("/var/www/boinc/sensor/html/inc/earthquake_email.inc");
-$mag  = 3.6; 
-$longitude = 172.494; 
-$latitude = -43.465; 
-$depth = 2.2; 
-$n_stations = 8; 
-$etime = 1379651011.251209; 
-$dtime = 1379651030; 
-$dt_detect  = 18.7; 
-$edir       = 1379651011; 
+$mag  = 3.5; 
+$longitude = -124.869; 
+$latitude = 37.274; 
+$depth = 39.8; 
+$n_stations = 7; 
+$etime = 1379816537.717194; 
+$dtime = 1379816624; 
+$dt_detect  = 86.3; 
+$edir       = 1379816537; 
 
 earthquake_email($mag,$longitude,$latitude,$depth,$n_stations,$etime,$edir,$dtime,$dt_detect);
 
