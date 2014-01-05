fa8885f4326e5a9d6201e4d863e52d0d3dfd7ec2
Carl Christensen <carlgt1@yahoo.com>
2014-01-05 10:00:29 -0500

    removed extraneous object files

--- client/bin/qcn.build/Debug/qcn.build/Objects-normal/i386/qcn_gzip.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/carlgt1/projects/qcn/client/mac_build/../util/qcn_gzip.cpp \
-  /Users/carlgt1/projects/qcn/client/mac_build/../../../boinc/lib/filesys.h \
-  ../util/qcn_gzip.h
--- client/bin/qcn.build/Debug/qcn.build/Objects-normal/x86_64/gzstream.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/carlgt1/projects/qcn/client/mac_build/../util/gzstream.cpp \
-  ../util/gzstream.h
