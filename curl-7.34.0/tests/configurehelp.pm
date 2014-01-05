6e1b4b5ee4afe27fe6bae0f2ce95dd1dc09efc38
Carl Christensen <carlgt1@yahoo.com>
2014-01-05 18:32:03 -0500

    new curl 7.34.0 and mac build file for xcode5

---
+++ curl-7.34.0/tests/configurehelp.pm
@@ -0,0 +1,23 @@
+# This is a generated file.  Do not edit.
+
+package configurehelp;
+
+use strict;
+use warnings;
+use Exporter;
+
+use vars qw(
+    @ISA
+    @EXPORT_OK
+    $Cpreprocessor
+    );
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(
+    $Cpreprocessor
+    );
+
+$Cpreprocessor = 'gcc -E -O2 -isystem /Applications/XCode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk/usr/include -isysroot /Applications/XCode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk -arch x86_64';
+
+1;
