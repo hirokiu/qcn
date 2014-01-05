a731e901ef3b1af40cc4eed7fca8c06967b22395
Carl Christensen <carlgt1@yahoo.com>
2014-01-04 23:42:05 -0500

    killer trickle update

--- server/sql/trickle_down_abort.sql
+++ server/sql/trickle_down_abort.sql
@@ -24,6 +24,18 @@ concat('<trickle_down>\n<result_name>',
     group by hostid,result_name
 ;
 
+/* killer trickle for hosts that are resending huge lists of triggers (>1000)
+
+insert into continual.msg_to_host
+(create_time,hostid,variety,handled,xml)
+select unix_timestamp(), hostid, 'abort', 0,
+concat('<trickle_down>\n<result_name>',
+  result_name,
+   '</result_name>\n<abort></abort>\n</trickle_down>\n')
+  from continual.qcn_trigger
+ group by hostid having count(*)>1000;
+
+
 
 /*
     this sends a 'killer trickle' for hosts running an older version than we'd like who triggered in the past two weeks
