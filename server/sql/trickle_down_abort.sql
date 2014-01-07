9bf74f6e055d94b8ec40bbf48518995bfc42ab22
Carl Christensen <carlgt1@yahoo.com>
2014-01-07 11:42:38 -0500

    update killer trickle sql file for hosts trickling too much (ie no sensor bg in v 7.33)

--- server/sql/trickle_down_abort.sql
+++ server/sql/trickle_down_abort.sql
@@ -24,6 +24,22 @@ concat('<trickle_down>\n<result_name>',
     group by hostid,result_name
 ;
 
+
+/* killer trickle for hosts with no sensor that are ping triggering too much (>100) */
+
+insert into msg_to_host
+(create_time,hostid,variety,handled,xml)
+select unix_timestamp(), hostid, 'abort', 0,
+concat('<trickle_down>\n<result_name>',
+  result_name,
+   '</result_name>\n<abort></abort>\n</trickle_down>\n')
+  from qcn_trigger 
+     where varietyid=1 and time_trigger>unix_timestamp()-259200 
+       and qcn_sensorid=0 group by hostid having count(*)>100;
+
+
+
+
 /* killer trickle for hosts that are resending huge lists of triggers (>1000)
 
 insert into continual.msg_to_host
