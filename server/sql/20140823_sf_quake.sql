888a946acf9198cd23b4eb122dee178d540338cc
Carl Christensen <carlgt1@cmc-mbp13.home>
2014-08-25 17:05:44 -0400

    more sf quake queries

--- server/sql/20140823_sf_quake.sql
+++ server/sql/20140823_sf_quake.sql
@@ -1,3 +1,6 @@
+// triggers within a minute of the quake and received in that window too
+select count(distinct hostid) from sensor.qcn_trigger where time_trigger between 1408875635 and 1408875700 and time_received between 1408875635 and 1408875700 and latitude between 37 and 39 and longitude between -123 and -121 and varietyid=0 and qcn_sensorid between 104 and 106;
+
 /*  all online usb sensor computers */
 
 select count(distinct hostid) from sensor.qcn_trigger where time_trigger between 1408874000 and 1408876600 and latitude between 37 and 39 and longitude between -123 and -121 and varietyid=1 and qcn_sensorid between 104 and 106;
